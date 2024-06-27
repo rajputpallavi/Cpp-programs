1808b212b5e24db202d24281b7421f73005f2e63
pallavi thakur <thakurpallavi560@gmail.com>
2024-06-27 11:47:05 +0530

    first program

---
+++ datatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"size of integer is "<<sizeof(a)<<endl;
+    float b;
+    cout<<"size of float is "<<sizeof(b)<<endl;
+    char c;
+    cout<<"size of character is "<<sizeof(c)<<endl;
+    bool d;
+    cout<<"size of boolean is "<<sizeof(d)<<endl;
+    return 0;
+}
