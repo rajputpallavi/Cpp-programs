0d1350d3c86f04ceed81a6cb854e723d68be35b9
pallavi thakur <thakurpallavi560@gmail.com>
2024-06-28 11:20:48 +0530

    new file added

---
+++ breakcontinue.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     //printing all numbers from 1 to 30 except the multiple of 3
+//     //we will use continue
+//     for(int i=1;i<=30;i++){
+//         if(i%3==0){
+//             continue;
+//         }
+       
+//         cout<<i<<endl;
+//     }
+// }
+ 
+// int main(){
+//     int n; int flag=1;
+//     //check if the number is prime or not
+//     cin>>n;
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//            flag=0;
+//             break;
+//     }
+//        }
+//        if(flag==1){
+//         cout<<"prime number"<<endl;
+//        }
+//        else{
+//         cout<<"not prime"<<endl;
+//        }
+// }
+
+//print all prime numbers from 1 to n;
+// int main(){
+//     int a,b;
+//     cin>>a;
+//     cin>>b;
+//      int flag;
+//     for(int i=a;i<=b;i++){
+//         int j;
+//         for( j=2;j<i;j++){
+//             if(i%j==0){
+//                 flag=0;
+//                 break;
+//             }
+//         flag=1;
+//         }
+//         if(flag==1){
+//          cout<<i<<endl;
+//         }
+    
+//     }
+// }
+
+
+
+//we use continue to skip to next iteration of loop
+//we use break statement to terminate the loop;
