a8adf095325422ba2e8dc97c4a4c03b5771c33d9
pallavi thakur <thakurpallavi560@gmail.com>
2024-06-28 11:49:39 +0530

    new file added

---
+++ switch.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     char a;
+//     cout<<"enter the character:";
+//     cin>>a;
+//     switch(a){
+//         case 'a':
+//         cout<<"Hello"<<endl;
+//         break;
+//         case 'b':
+//         cout<<"Namaste"<<endl;
+//         break;
+//         case 'c':
+//         cout<<"Hola"<<endl;
+//         break;
+//         case 'd':
+//         cout<<"Ciao"<<endl;
+//         break;
+//         case 'e':
+//         cout<<"Bonjour"<<endl;
+//         break;
+//         case 'f':
+//         cout<<"Hi"<<endl;
+//         break;
+//         default:
+//         cout<<"i am still learning"<<endl;
+//         break;
+//         }
+// }
+
+// int main()
+// {
+//     int a, b;
+//     cout << "enter two numbers" << endl;
+//     cin >> a >> b;
+//     char op;
+//     cout << "enter the operator" << endl;
+//     cin >> op;
+//     switch (op)
+//     {
+//     case '+':
+//     cout<<a+b;
+//     break;
+//     case '-':
+//     cout<<a-b;
+//     break;
+//     case '*':
+//     cout<<a*b;
+//     break;
+//     case '%':
+//     cout<<a%b;
+//     break;
+//     case '/':
+//     cout<<a/b;
+//     break;
+//     default:
+//     cout<<"enter a valid operator"<<endl;
+//     break;
+//     }
+// }
